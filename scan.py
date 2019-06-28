c3c88b8277f4d7dcf1bff67e0dda4d329a9468ce
Matt Clarke <matt.clarke@esss.se>
2019-06-28 11:36:47 +0200

    Uses intervals

--- scan.py
+++ scan.py
@@ -1,6 +1,6 @@
 import sys
 # TODO: Set this path to where the fbschemas folder is
-sys.path.append("/Users/mattclarke/Desktop/nicos_scan")
+sys.path.append("/hzb/caress/mike/v20_nicos_scanning")
 
 from kafka import KafkaProducer, KafkaConsumer, TopicPartition
 import time
@@ -9,31 +9,48 @@ import fbschemas.hs00.EventHistogram as EventHistogram
 import fbschemas.hs00.ArrayDouble as ArrayDouble
 from fbschemas.hs00.Array import Array
 import numpy as np
+from math import sqrt
+from datetime import datetime
+import random
 
 
-# TODO: Edit these settings as appropriate
-POSITIONS_1 = [0, 1, 2, 3, 4, 5]
-POSITIONS_2 = [0, 1, 2, 3, 4, 5]
-NICOS_MOTOR_1 = "m1"
-NICOS_MOTOR_2 = "m2"
-KAFKA_ADDRESS = ["localhost:9092"]
+# TODO: Edit these scan settings as appropriate
+SCAN_LOCATION = "/hzb/caress/mike/v20_nicos_scanning/scans/"
+SCAN_NAME = "slit3_vertical"
+NICOS_MOTOR = "slit3v_center"
+POSITIONS = np.arange(-5, 5, 0.1)
+COLUMNS = [  # Extra columns of interest; Counts and Error always added later
+    ("CenterX", slit3h_center, "%.3f"),
+    ("CenterY", slit3v_center, "%.3f"),
+    ("Width", slit3h_gap, "%.3f"),
+    ("Height", slit3v_gap, "%.3f"),
+]
+COUNT_TIME_SECS = 10
+FILEWRITER = False
+
+# TODO: Kafka settings
+KAFKA_ADDRESS = ["192.168.1.80:9092"]
 JUST_BIN_IT_COMMAND_TOPIC = "hist_commands"
-EVENT_TOPIC = "LOQ_events"
+EVENT_TOPIC = "denex_detector"
 HISTOGRAM_TOPIC = "hist_topic"
-COUNT_TIME_SECS = 5
 
-CONFIG = {
-    "data_brokers": KAFKA_ADDRESS,
-    "data_topics": [EVENT_TOPIC],
-    "histograms": [
-        {
-            "type": "sehist1d",
-            "tof_range": [0, 100_000_000],
-            "num_bins": 50,
-            "topic": HISTOGRAM_TOPIC,
-        }
-    ],
-}
+
+def generate_config(id, count_time):
+    return {
+        "cmd": "config",
+        "data_brokers": KAFKA_ADDRESS,
+        "data_topics": [EVENT_TOPIC],
+        "histograms": [
+            {
+                "type": "sehist1d",
+                "tof_range": [0, 100_000_000],
+                "num_bins": 50,
+                "topic": HISTOGRAM_TOPIC,
+                "interval": count_time,
+                "id": id
+            }
+        ],
+    }
 
 
 def deserialise_hs00(buf):
@@ -86,67 +103,125 @@ def deserialise_hs00(buf):
     return hist
 
 
-def get_total_counts(consumer, topic):
-    data = {}
-    consumer.seek_to_end(topic)
-
-    while len(data) == 0:
-        data = consumer.poll(5)
-    ans = deserialise_hs00(data[topic][-1].value)
-    return sum(ans["data"])
-
-
-def do_scan():
-    # Configure Kafka and just-bin-it
+def send_config(config):
     producer = KafkaProducer(bootstrap_servers=KAFKA_ADDRESS)
-    producer.send(JUST_BIN_IT_COMMAND_TOPIC, bytes(json.dumps(CONFIG), "utf-8"))
+    producer.send(JUST_BIN_IT_COMMAND_TOPIC, bytes(json.dumps(config), "utf-8"))
     producer.flush()
     time.sleep(2)
 
+
+def get_total_counts(consumer, topic, id, seconds):
+    config = generate_config(id, seconds)
+    send_config(config)
+
+    data = {}
+    consumer.seek_to_end(topic)
+    pos = consumer.position(topic)
+    if pos == 0:
+        raise Exception("Could not find data in topic")
+
+    consumer.seek(topic, pos - 1)
+
+    while True:
+        while len(data) == 0:
+            data = consumer.poll(5)
+        ans = deserialise_hs00(data[topic][-1].value)
+        data = {}
+        status = json.loads(ans["info"])
+        if status["id"] == id and status["state"] == "FINISHED":
+            return sum(ans["data"])
+
+
+def fw_isbusy():
+    return NexusFileWriter.curstatus[0] != 200
+
+
+def fw_startandwait():
+    start_filewriter()
+    while not fw_isbusy():
+        sleep(0.1)
+    print(NexusFileWriter.curstatus[1])
+
+
+def fw_stopandwait():
+    stop_filewriter()
+    while fw_isbusy():
+        sleep(2)
+
+
+def do_scan():
     consumer = KafkaConsumer(bootstrap_servers=KAFKA_ADDRESS)
     topic = TopicPartition(HISTOGRAM_TOPIC, 0)
     consumer.assign([topic])
     consumer.seek_to_end(topic)
 
-    histogram = []
-    last_value = 0
+    outputs = []
+    columns = []
 
-    # TODO: start filewriter
-    # start_filewriter()
+    # Start filewriter
+    if FILEWRITER:
+        fw_startandwait()
 
-    for i in POSITIONS_1:
-        # Move motor 1 to position
-        print("Moving 1 to {}...".format(i))
-        maw(NICOS_MOTOR_1, i)
+    for pos in POSITIONS:
+        # Move motor to position
+        print("Moving to {}...".format(pos))
+        maw(NICOS_MOTOR, pos)
 
-        for j in POSITIONS_2:
-            # Move motors to position
-            print("Moving 2 to {}...".format(j))
-            maw(NICOS_MOTOR_2, j)
-            last_value = get_total_counts(consumer, topic)
-            print("Value after move = {}".format(last_value))
+        # Read actual positions
+        columns = []
+        for n, device, f in COLUMNS:
+            columns.append(device.read())
 
-            # Collect data for some number of seconds
-            print("Counting for {} seconds...".format(COUNT_TIME_SECS))
-            time.sleep(COUNT_TIME_SECS)
+        # Collect data for some number of seconds
+        # Note: may take longer in "real time" than expected due to delays in data
+        # processing
+        print("Counting for {} seconds of data...".format(COUNT_TIME_SECS))
+        id = "id-{}".format(random.randint(1, 10000000))
+        counts = get_total_counts(consumer, topic, id, COUNT_TIME_SECS)
 
-            # Get total counts
-            next_value = get_total_counts(consumer, topic)
-
-            # Counts for "data collection" is current count minus the counts after move
-            histogram.append(next_value - last_value)
-            last_value = next_value
-            print("Value after counting = {}".format(last_value))
-
-    # TODO: stop filewriter
-    # stop_filewriter()
+        error = sqrt(counts)
+        columns.append(counts)
+        columns.append(error)
+        outputs.append(columns)
+        print(columns)
 
     # Print results
-    row = 0
-    num_rows = len(histogram) / len(POSITIONS_1)
-    while row < num_rows:
-        start = row * len(POSITIONS_1)
-        print(histogram[start : start + len(POSITIONS_2)])
-        row += 1
+    print(outputs)
+
+    # Pretty print
+    headers = [name for name, d, f in COLUMNS]
+    headers.append("Counts")
+    headers.append("Error")
+
+    format = [f for n, d, f in COLUMNS]
+    format.append("%d")  # Counts
+    format.append("%.3f")  # Error
+
+    data = []
+    for row in outputs:
+        data.append([f % (v,) for f, v in zip(format, row)])
+
+    width = max(len(x) for x in headers)
+    width = max(width, *[len(str(value)) for value in row for row in data])
+    width = width + 4
+
+    for row in [headers] + data:
+        print("".join(value.ljust(width) for value in row))
+
+    # Save results
+    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+    file = open(SCAN_LOCATION + "%s_%s.dat" % (SCAN_NAME, timestamp), "w")
+
+    file.write("\t".join(headers) + "\n")
+    format = "\t".join(format) + "\n"
+    for row in outputs:
+        file.write(format % tuple(row))
+
+    file.close()
+
+    # Stop filewriter
+    if FILEWRITER:
+        fw_stopandwait()
+
 
 do_scan()
